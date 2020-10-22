e8ccef500c9d4ca979df333cd3bb97a534581652
alexane.jouglar <alexane.jouglar@localhost>
2020-10-22 10:40:45 +0200

    NbJours

--- Programmation Objet et Groupware Alexane/src/atelier05/nbJours.java
+++ Programmation Objet et Groupware Alexane/src/atelier05/nbJours.java
@@ -1,21 +1,22 @@
 package atelier05;
 
 import cours04.Date;
-import java.text.SimpleDateFormat;
 
 public class nbJours {
-	
+
 	public static void main(String[] args) {
-		Date df = new Date(14,7,2021);
-		     try {
-		        Date dateAvant = df.parse("22,10,2020");
-		       Date dateApres = df.parse("14,7,2021");
-		       long diff = dateApres.getTime() - dateAvant.getTime();
-		       float res = (diff / (1000*60*60*24));
-		       System.out.println("Nombre de jours entre les deux dates est: "+res);
-		     } catch (Exception e) {
-		         e.printStackTrace();
-		     }
-		   }
+		Date fete = new Date(14, 7, 2021);
+		Date aujourdhui = new Date(22, 10, 2020);
+
+		int nb = 0;
+
+		while (aujourdhui.CompareTo(fete) == true) {
+			aujourdhui.Incrementer();
+			nb++;
+		}
+
+		System.out.println(nb);
+
+	}
 
 }
