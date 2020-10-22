b2fda451c15ef2cbacfaab1c0b1cb7437d75c3b6
alexane.jouglar <alexane.jouglar@localhost>
2020-10-22 10:04:32 +0200

    x

---
+++ Programmation Objet et Groupware Alexane/src/cours04/Date.java
@@ -0,0 +1,19 @@
+package cours04;
+
+public class Date {
+
+	public Date(int i, int j, int k) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Date parse(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getTime() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
