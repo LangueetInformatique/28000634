c84a646d551d1453b9dd5467f6da52b603acef51
alexane.jouglar <alexane.jouglar@localhost>
2020-11-12 10:52:41 +0100

    x

---
+++ Programmation Objet et Groupware Alexane/src/atelier07/Calendar2.java
@@ -0,0 +1,61 @@
+package atelier07;
+
+import java.util.Calendar;
+
+public class Calendar2 extends Calendar {
+	
+	//Créer la  classe  Calendar2  dérivée  de  Calendar  implémentant 
+	// attribut cours et trois nouvelles méthodes creation, afficher, duree
+	// attribut cours est de type boolean et contient vrai si l’objet est créé pendant le cours (jeudi 9h30 -12h)
+	// la méthode  creation()  affichera  la  date  et  l’heure  de la  création  de  l’objet,  
+	// la méthode la date et l’heure courante, la méthode duree() affiche le nombre de secondes depuis la création de l'objet
+	
+	@Override
+	public void add(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void computeFields() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void computeTime() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getGreatestMinimum(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLeastMaximum(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaximum(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMinimum(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void roll(int arg0, boolean arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Programmation Objet et Groupware Alexane/src/atelier07/ExampleGregorianDate.java
@@ -0,0 +1,13 @@
+package atelier07;
+
+import java.util.Date;
+
+public class ExampleGregorianDate {
+	
+	public static void main(String[] args) {
+		
+		Date date = new Date();
+		System.out.println(date.toString());
+	}
+
+}
