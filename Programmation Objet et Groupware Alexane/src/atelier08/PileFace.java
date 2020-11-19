d3f92c8e7ad7f39f34892d3d89a4342d8ce2a27e
alexane.jouglar <alexane.jouglar@localhost>
2020-11-19 10:28:13 +0100

    Essai

---
+++ Programmation Objet et Groupware Alexane/src/atelier08/PileFace.java
@@ -0,0 +1,16 @@
+package atelier08;
+
+public class PileFace {
+	
+	public static void  main(String[] args) {
+	
+	int piece = 0;
+	if (piece == 1) {
+	    System.out.println("pile");
+	}
+	if (piece == 2) {
+	    System.out.println("face");
+	}
+	
+}
+	}
