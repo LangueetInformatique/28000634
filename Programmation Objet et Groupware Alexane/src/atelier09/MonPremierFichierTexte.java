842160cee2bdc478196365ca6ffe4351401e5ee2
alexane.jouglar <alexane.jouglar@localhost>
2020-11-26 10:10:45 +0100

    NewCommit

--- Programmation Objet et Groupware Alexane/src/atelier09/MonPremierFichierTexte.java
+++ Programmation Objet et Groupware Alexane/src/atelier09/MonPremierFichierTexte.java
@@ -1,10 +1,11 @@
 package atelier09;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-
-import util.Keyboard;
 
 public class MonPremierFichierTexte {
 	/*
@@ -12,23 +13,42 @@ public class MonPremierFichierTexte {
 	qui écrit dans le fichier texte « Nom.txt » les noms des membres du groupe. 
 	*/
 
-	public static void main (String[] args) {
+	public static void main (String[] args) throws FileNotFoundException {
 		String ligne;
+		String nfich = "src/atelier09/Nom.txt";
 		try {
-			File f = new File(Keyboard.getString("Nom.txt"));
+			File f = new File(nfich);
 			
 			PrintWriter pr = new PrintWriter(f);
-			pr.print("Morgann");
-			pr.print("Julien");
-			pr.print("Mustapha");
-			pr.print("Chenwangli");
-			pr.print("Ana");
-			pr.print("Joyce");
+			pr.println("Morgann");
+			pr.println("Julien");
+			pr.println("Mustapha");
+			pr.println("Chengwanli");
+			pr.println("Ana");
+			pr.println("Joyce");
 			
 			pr.close();
 			System.out.println("Fin d'écriture fichier");
-		
+		}
+		finally {
+		/* 
+		 * Exercice 2 : Ajouter à la classe MonPremierFichierTexte la méthode Verifier 
+		 * qui vérifie si le fichier « Nom.txt » contient bien les noms des membres du groupe.
+		 */
+
+		try {
+			File f = new File(nfich);
+			FileReader fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+			while((ligne=br.readLine()) !=null) {
+				System.out.println(ligne);
+			}
+			br.close();
+		}
+		catch (IOException e) {e.printStackTrace();}
+
 	}
-		catch (IOException e) {e.printStackTrace(); }
+		}
 }
-}
+
+
