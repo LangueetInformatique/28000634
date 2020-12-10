4ca56908e2c6d3478e27af030eb5926fe4d7e3a5
alexane.jouglar <alexane.jouglar@localhost>
2020-12-10 09:42:17 +0100

    Début

---
+++ Programmation Objet et Groupware Alexane/src/atelier11/MaPremiereFenetre.java
@@ -0,0 +1,17 @@
+package atelier11;
+
+import javax.swing.*;
+
+public class MaPremiereFenetre {
+	
+	/** Ecrire une classe exécutable créant une fenêtre dont le titre sera votre nom et
+	contenant un panneau de la couleur de votre choix. 
+	
+	Ce panneau contiendra un menu dont le titre sera prénoms et dont les sous-menus seront vos prénoms. **/
+
+	public static void main(String args[]) {
+		JFrame Alexane = new JFrame();
+		Alexane.setVisible(true);
+		Alexane.setBackground();
+	}
+}
