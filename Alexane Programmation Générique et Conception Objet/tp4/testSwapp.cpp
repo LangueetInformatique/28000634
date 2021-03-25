1e7fe83210a52c306e1e53d65d1cd86f6b6662b4
Alexane J <alexanetouti@hotmail.Fr>
2021-03-25 12:19:21 +0100

    swap

---
+++ Alexane Programmation Générique et Conception Objet/tp4/testSwapp.cpp
@@ -0,0 +1,69 @@
+//#include "swap.h"
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+
+void swap (int *a, int *b) {
+
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+	/** passage par valeur**/
+}
+
+void swap2 (int &a, int &b) {
+
+	int c;
+	c = a;
+	a = b;
+	b = c;
+
+}
+int main() {
+	/**
+	 * int a = 10;
+	int a;
+	cout << "Veuillez entrer un entier: " << endl;
+	cin >> a ;
+	//int b = 20;
+	int b;
+	cout << "Veuillez entrer un entier:" << endl;
+	cin >> b ;
+	int c;
+
+	cout << "Avant swap: " << a << " " << b << endl;
+
+	c = a;
+	a = b;
+	b = c;
+
+	cout << "Après swap: " << a << " "<< b << endl;**/
+
+	/**int a;
+	int b;
+	cout << "Veuillez entrer deux entiers: " << endl;
+	cin >> a;
+	cin >> b;
+	cout << "Avant swap: " << a << " " << b << endl;
+	swap(&a, &b);
+	cout << "Après swap: " << a << " "<< b << endl;
+**/
+
+	int x;
+	int y;
+	cout << "Veuillez entrer deux entiers: " << endl;
+	cin >> x;
+	cin >> y;
+	cout << "Avant swap: " << x << " " << y << endl;
+	swap2(x, y);
+	cout << "Après swap: " << x << " "<< y << endl;
+
+	return 0;
+
+}
+
+
+
