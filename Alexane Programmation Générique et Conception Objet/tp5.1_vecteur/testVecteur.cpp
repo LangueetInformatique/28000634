74e7b17e058ef60e2b4ff1cca752c048e720f455
Alexane J <alexanetouti@hotmail.Fr>
2021-04-08 12:35:31 +0200

    ajouts

--- Alexane Programmation Générique et Conception Objet/tp5.1_vecteur/testVecteur.cpp
+++ Alexane Programmation Générique et Conception Objet/tp5.1_vecteur/testVecteur.cpp
@@ -2,7 +2,28 @@
 
 int main() {
 
-	vector<int> tableau(5);
+	int t[5] = {1, 2, 3, 4};
 
+	for (int i = 0; i<5; i++){
+		cout<<t[i]<<endl;
+	}
+
+	vector<int> tableau(5);
+	tableau[0] = 10;
+	tableau[1] = 20;
+	tableau[2] = 30;
+	tableau[3] = 40;
+	tableau[4] = 50;
+	tableau[5] = 60;
+
+	cout <<tableau.front() <<endl;
+	cout <<tableau.back() <<endl;
+
+	tableau.push_back(80); //pour ajouter des éléments à la fin
+	tableau.erase(tableau.begin()+2); // pour supprimer l'élément du vecteur d'indice 2
+
+	for (int i=0; i<tableau.size(); i++){
+		cout << tableau[i]<<endl;
+	}
 	return 0;
 }
