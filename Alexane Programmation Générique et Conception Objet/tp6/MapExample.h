44197b4ecad5c384835ad25a838776c10e27c558
Alexane J <alexanetouti@hotmail.Fr>
2021-05-06 12:57:22 +0200

    Map

--- Alexane Programmation Générique et Conception Objet/tp6/MapExample.h
+++ Alexane Programmation Générique et Conception Objet/tp6/MapExample.h
@@ -1,32 +1,57 @@
-
 #include <map>
 #include <iostream>
 #include <string>
+#include <fstream> // pour lire le fichier
 
 using namespace std;
 
-/**typedef map <string, float, less<string> > mStringFloat;
-typedef mStringFloat::iterator imStringFloat;
-
-void prix(mStringFloat prixFruit, string fruit) {
-		imStringFloat it;
-		if ((it = prixFruit.find(fruit)) == prixFruit.end())
-			cout << "fruit non référencé"; else cout << it->second;
-		cout << endl;
-	}**/
-
 void EssaisMap() {
 
-		map <string, string> a;
+/**		map <string, string> a;
 		a["prenom"]= "Alexane";
-		cout << a["prenom"];
-
-/**		mStringFloat prixFruit;
-		prixFruit.insert(make_pair("poire", 1.5));
-		prixFruit.insert(make_pair("pêche", 2.7));
-		prixFruit.insert(make_pair("orange", 1.2));
-		prix(prixFruit, "poire");
-		prix(prixFruit, "pomme");
-		**/
+		cout << a["prenom"];**/
+		ifstream monFlux("mots.txt");  //Ouverture d'un fichier en lecture
+		string mot;
+		map <string, int> counter;
+		while (monFlux >> mot) {
+				++counter[mot];
+			}
+/**		for(auto & pairELement : counter) {
+				cout << pairElement.first << " ";
+				cout << pairElement.second << endl;
+		}**/
 
 }
+
+/**    ifstream myFile;
+    myFile.open("mots.txt");
+    if(!myFile.is_open()) cout << 1;
+    map<string, map<string, int> > categories;
+    while(myFile.good())
+    {
+        string s;
+        getline(myFile, s);
+        int pos = s.find_first_of(' ');
+        if(pos < 0) continue;
+        string word = s.substr(0, pos);
+        string category = word;
+        s = s.erase(0, pos+1);
+        while(s.size() > 0)
+        {
+            pos = s.find_first_of(' ');
+            if(pos < 0)
+                pos = s.size();
+            string word = s.substr(0, pos);
+            if(word != "")
+                categories[category][word]++;
+            s = s.erase(0, pos+1);
+        }
+    }
+    for(map<string, map<string, int> >::iterator cit = categories.begin(); cit != categories.end(); ++cit)
+    {
+        cout << "Category - " << cit->first << endl;
+        for(map<string, int>::iterator wit = cit->second.begin(); wit != cit->second.end(); ++wit)
+            cout << "\tword: " << wit->first << ",\t" << wit->second << endl;
+
+**/
+
